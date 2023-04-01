d2099e9a8b5a1182e1682046f912cf1491086458
Chrystian Ruan <chrystianr37@gmail.com>
2023-04-01 01:22:20 -0300

    alterando consulta sql no endpoint produtos

--- app/Http/Controllers/api/ProductController.php
+++ app/Http/Controllers/api/ProductController.php
@@ -9,7 +9,7 @@ use App\Models\Produto;
 class ProductController extends Controller
 {
     public function index() {
-        $produtos = Produto::select("produtos.id", "produtos.imagens", "produtos.nome", "produtos.descricao", "produtos.preco", "produtos.desconto", "produtos.estoque", "categorias.nome_categoria as categoria", "tipos.nome_tipo as tipo", "marcas.nome_marca as marca")
+        $produtos = Produto::select("produtos.*", "categorias.nome_categoria as categoria", "tipos.nome_tipo as tipo", "marcas.nome_marca as marca")
         ->join("categorias", "categorias.id", "=", "produtos.categoria_id")
         ->join("tipos", "tipos.id", "=", "produtos.tipo_id")
         ->join("marcas", "marcas.id", "=", "produtos.marca_id")
